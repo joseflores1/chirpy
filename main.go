8d892b0da202661e0ce108c48bb3a863dbbd98f3
joseflores1 <joseflores@alumnos.uai.cl>
2025-05-30 17:39:11 -0400

    Add some content to serve and readiness endpoint

--- main.go
+++ main.go
@@ -7,16 +7,25 @@ import (
 
 func main() {
 
+	const rootPath = "."
 	const port = "8080"
 
-	// Define server struct and its handler
+	// Define server structs and handlers
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(rootPath))))
+	serveMux.HandleFunc("/healthz", handlerReadiness)
 	serverStruct := &http.Server{
 		Handler: serveMux,
-		Addr: ":" + port,
-	}	
+		Addr:    ":" + port,
+	}
 
 	// Start the server
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", rootPath, port)
 	log.Fatal(serverStruct.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
