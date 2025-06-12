48ee59763a5ff388309a110649d2af2f74805771
joseflores1 <joseflores@alumnos.uai.cl>
2025-06-12 16:27:04 -0400

    Add sqlc generated code and database schemas & queries

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	"github.com/joseflores1/chirpy/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	queries        *database.Queries
 }
 
 func main() {
+
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, errOpen := sql.Open("postgres", dbURL)
+	if errOpen != nil {
+		log.Fatal("Couldn't open connection to database: ", errOpen)
+	}
+	dbQueries := database.New(db)
 
 	// Define consts
 	const rootPath = "."
@@ -19,6 +34,7 @@ func main() {
 	// Define apiConfig struct
 	apiCfg := &apiConfig{
 		fileServerHits: atomic.Int32{},
+		queries:        dbQueries,
 	}
 
 	// Define mux
@@ -39,6 +55,6 @@ func main() {
 	}
 
 	// Start the server
-	log.Printf("Serving files from %s on port: %s\n", rootPath, port)
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(serverStruct.ListenAndServe())
 }
